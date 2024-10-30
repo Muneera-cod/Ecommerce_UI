89252e618c57aee1f9cf8063c806b8a62f9a742f
Muneera <muneerakottarath99@gmail.com>
2024-10-30 11:50:17 +0530

    bannergrid updated again

--- src/Components/ui/Home/Banners/SmallBanner.jsx
+++ src/Components/ui/Home/Banners/SmallBanner.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import macbookpro from '../../../../assets/Images/Macbook 1.png'
 import playStation from '../../../../assets/Images/PlayStation.png'
 import AirPod from '../../../../assets/Images/Headphone.png'
+import AirPod1 from '../../../../assets/Images/AirpodApple.png'
+
 import AppleVissionPro from '../../../../assets/Images/Graphite (1).png'
+import AppleVissionPro1 from '../../../../assets/Images/ApplevissionPro.png'
 function SmallBanner() {
   return (
     <div className='flex sm:flex-col lg:flex-row '>
@@ -16,14 +19,20 @@ function SmallBanner() {
            </div>
            <div className='flex  sm:flex-col xl:flex-row items-stretch h-full w-full'>
                <div className='flex-1 flex bg-secondaryClr sm:flex-col lg:flex-row sm:px-[16px] lg:px-[0] sm:py-[40px] lg:py-[0] gap-[24px] items-center '>
-                  <div lassName=' basis-1/2 flex items-center justify-center '><img src={AirPod} className='max-w-[192px] h-[200px] overflow-hidden' ></img></div>
+                  <div lassName=' basis-1/2 flex items-center justify-center '>
+                  <img src={AirPod} className='sm:block xl:hidden max-w-[192px] h-[200px] overflow-hidden' ></img>
+                  <img src={AirPod1} className='sm:hidden xl:block max-w-[192px] h-[200px] overflow-hidden' ></img>
+
+                  </div>
                   <div className='basis-1/2 flex flex-col sm:items-center lg:items-start justify-center sm:p-[24px] lg:p-[48px] sm:text-center lg:text-left'>
                     <p className='sm:text-[34px] lg:text-[29px] font-[500] leading-[40px] '>Apple AirPods Max</p>
                     <p className='text-secondaryTxt text-[14px] font-[500] leading-[24px] line-clamp-3'>An immersive way to experience entertainment</p>
                   </div>
                </div>
                <div className='flex-auto flex bg-[#353535] sm:flex-col lg:flex-row sm:px-[16px] lg:px-[0] sm:py-[40px] lg:py-[0] gap-[24px]  items-center '>
-                  <img src={AppleVissionPro} className='w-[325.867px] h-[192.445px]'></img>
+                  <img src={AppleVissionPro} className='sm:block xl:hidden w-[325.867px] h-[192.445px]'></img>
+                  <img src={AppleVissionPro1} className='sm:hidden xl:block '></img>
+
                   <div className='flex flex-col sm:items-center lg:items-start justify-center sm:text-center lg:text-left sm:pl-[24px] lg:pl-[48px] sm:pr-[24px] lg:pr-[48px] items-center'>
                     <p className='sm:text-[34px] lg:text-[29px] font-[500] leading-[40px]'>Apple 
                     Vision Pro</p>
